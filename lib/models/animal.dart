5f85a8f058f2285522be97ba79896ad4cda1a262
0r4atmqetptfjoa-oss <0r4atmqetptfjoa@gmail.com>
2025-09-19 23:18:18 +0300

    masiv update

--- lib/models/animal.dart
+++ lib/models/animal.dart
@@ -2,8 +2,9 @@ class AnimalItem {
   final String name;
   final String category;
   final String image;
-  const AnimalItem({required this.name, required this.category, required this.image});
+  final String? audio;
+  const AnimalItem({required this.name, required this.category, required this.image, this.audio});
 
   factory AnimalItem.fromJson(Map<String, dynamic> j) =>
-      AnimalItem(name: j['name'], category: j['category'], image: j['image']);
+      AnimalItem(name: j['name'], category: j['category'], image: j['image'], audio: j['audio']);
 }
